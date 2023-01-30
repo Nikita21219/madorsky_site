383bd0d78ecabb104cddffd96937a5146705b164
Nikita Madorsky <madorskii@mail.ru>
2023-01-30 11:22:05 +0300

    stable

---
+++ requirements/wordpress/conf/wp-config.sh
@@ -0,0 +1,15 @@
+#!bin/sh
+
+echo "<?php"
+echo "define( 'DB_NAME', '${DB_NAME}' );"
+echo "define( 'DB_USER', '${DB_USER}' );"
+echo "define( 'DB_PASSWORD', '${DB_PASS}' );"
+echo "define( 'DB_HOST', 'db' );"
+echo "define( 'DB_CHARSET', 'utf8' );"
+echo "define( 'DB_COLLATE', '' );"
+echo "define('FS_METHOD','direct');"
+echo "\$table_prefix = 'wp_';"
+echo "define( 'WP_DEBUG', false );"
+echo "if ( ! defined( 'ABSPATH' ) ) {"
+echo "define( 'ABSPATH', __DIR__ . '/' );}"
+echo "require_once ABSPATH . 'wp-settings.php';"
